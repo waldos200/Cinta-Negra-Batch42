8bc260412d74f0783c39470aa97bfc2af08fff6c
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-24 18:49:54 -0600

    API auth API Key

--- 01-Repaso/index.js
+++ 01-Repaso/index.js
@@ -19,3 +19,17 @@ request.get(BR_BA_QUOTES, (err, res, body) => {
         console.log(json[0].author);
     } else console.log(res.statusCode, err);
 });
+
+const getBandInfo = (band) => {
+    const AUDIO_DB = `https://theaudiodb.com/api/v1/json/miLlaveUnica/search.php?s=${band}`;
+    request.get(AUDIO_DB, (err, res, body) => {
+      if (err) console.log(err);
+      if (res.statusCode === 200) {
+        const json = JSON.parse(body);
+        console.log(json.artists[0].strArtist);
+      }
+      else console.log(res.statusCode, err);
+    });
+  }
+  
+  getBandInfo('Simon and Garfunkel');
