87b2439cae9a8da7a2fea36e8f3ff1620e7a668c
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-26 20:13:37 -0600

    agregar esquema y modelo de base de datos para sistema de control de vuelos

--- 03-Base-de-Datos/index.js
+++ 03-Base-de-Datos/index.js
@@ -8,8 +8,36 @@ const PORT = process.env.PORT || 3000; // Configurar variables de entorno con "p
 // Conexion a Base de Datos
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology:true })
-    .then(() => console.log('Database connected'))
+    .then(() => console.log('Database connected!'))
     .catch(() => console.log('Error connecting to database...'));
+
+// ODM -> Object Document Mapping esto es para mongoose
+// ORM -> Object Relational Mapping esto para una base de datos con sql
+
+/* 
+    Ejercicio de diseño de ase de datos
+    Un aeropuerto busca controlar los vuelos que llegan al lugar, desea conocer los vuelos que existen, a qué aerolínea pertenecen, las características del avión y el lugar de procedencia. Ayuda al aeropuerto a solucionar su problema.
+*/
+
+// Generar un esquema -> Definicion de las reglas de una coleccion 
+const flightsSchema = new mongoose.Schema({
+    airline: {
+        type: String,
+        required: true,
+    },
+    aircraft_name: {
+        type: String,
+        required: true,
+    },
+    aircraft_model: Number,
+    flight_from: {
+        type: String,
+        required: true,
+    },
+});
+
+// Generar un modelo a partir del esquema -> Objeto que nos permite interactuar con la coleccion
+const Flights = mongoose.model('Flights', flightsSchema);
 
 // Endpoints
 api.get('/', (req, res) => res.status(200).json({ massage: "it's alive!!!" }));
