bdeb36da44604d85d8927f4b4b94e350272710b0
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-29 19:43:59 -0600

    agregar create del crud de productos

--- 04-API-Referencias/index.js
+++ 04-API-Referencias/index.js
@@ -9,8 +9,15 @@ api.use(express.json({ extends: true }));
 
 const Products = require('./models/Products');
 
-// CRUD Products
-
 api.get('/', (req, res) => res.json({ message: "It's alive" }));
+
+// CRUD Products
+api.post('/api/products', (req, res) => {
+    const { body } = req;
+    const newProduct = new Products(body);
+    newProduct.save()
+        .then(mongoRes => res.status(201).json(mongoRes))
+        .catch(err => res.status(400).json(err));
+})
 
 api.listen(PORT, () => console.log(`Listening on ${PORT}`));
