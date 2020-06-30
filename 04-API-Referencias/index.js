b5d70e836e65eeaf0dc47c17d74b5cff60078dd2
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-29 19:34:24 -0600

    Agregar configuracion base con api con express

--- 04-API-Referencias/index.js
+++ 04-API-Referencias/index.js
@@ -1,2 +1,16 @@
 require('dotenv').config();
 require('./mongoClient/index');
+const express = require('express');
+const api = express();
+const PORT = process.env.PORT || 3000;
+
+api.use(express.urlencoded({ extends: true }));
+api.use(express.json({ extends: true }));
+
+const Products = require('./models/Products');
+
+// CRUD Products
+
+api.get('/', (req, res) => res.json({ message: "It's alive" }));
+
+api.listen(PORT, () => console.log(`Listening on ${PORT}`));
