7b270f6db221e79dbce97930a9266911bcdfd23d
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-29 19:19:36 -0600

    agregar schema y modelo de productos en su propia carpeta

---
+++ 04-API-Referencias/models/Products.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    price: {
+        type:Number,
+        requiered: true,
+    },
+    stock: {
+        type: Number,
+        default: 25,
+    },
+});
+
+const Products = mongoose.Model('Products', productSchema);
+
+module.exports = Products;
