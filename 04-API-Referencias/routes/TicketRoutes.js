0320cb06b2c49f4ed5e57d381a7da2a333bac7ae
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-30 18:10:48 -0600

    rutas de tickets

---
+++ 04-API-Referencias/routes/TicketRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+const Tickets = require('../models/Tickets');
+
+router.post('/api/tickets', (req, res) => {
+  const { body } = req;
+  const newTicket = new Tickets(body);
+  newTicket.save()
+    .then(mongoRes => res.status(201).json(mongoRes))
+    .catch(err => res.status(400).json(err));
+});
+
+router.get('/api/tickets', (req, res) => {
+  Tickets.find()
+    .then(mongoRes => res.status(201).json(mongoRes))
+    .catch(err => res.status(400).json(err));
+});
+
+router.get('/api/tickets/:id', (req, res) => {
+  const { id } = req.params;
+  Tickets.findById(id)
+    .then(mongoRes => res.status(200).json(mongoRes))
+    .catch(err => res.status(400).json(err));
+});
+
+router.patch('/api/tickets/:id', (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  Tickets.findByIdAndUpdate(id, body, { new: true })
+    .then(mongoRes => res.status(200).json(mongoRes))
+    .catch(err => res.status(400).json(err));
+});
+
+router.delete('/api/tickets/:id', (req, res) => {
+  const { id } = req.params;
+  Tickets.findByIdAndDelete(id)
+    .then(mongoRes => res.status(204).json(mongoRes))
+    .catch(err => res.status(400).json(err));
+});
+
+module.exports = router;
